b12a85a0ae8832c1ae38d17f0c0aef2ab6150775
Kasper Peulen <kasperpeulen@gmail.com>
2025-06-18 14:32:25 +0200

    implement @vitest/mocker in a storybook project

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+import { mockerPlugin } from "@vitest/mocker/node";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -6,6 +8,23 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  viteFinal: (config) => {
+    return mergeConfig(config, {
+      plugins: [
+        mockerPlugin({
+          // this filter is needed because otherwise the plugin
+          // is transforming code to references globalThis["__vitest_mocker__"]
+          // before that reference is being added by registerModuleMocker
+          filter: (id) => !id.includes("@vitest/mocker"),
+          hoistMocks: {
+            hoistedModule: "#vi",
+          },
+        } as Parameters<typeof mockerPlugin>[0] & {
+          filter: (id: string) => boolean;
+        }),
+      ],
+    });
   },
 };
 export default config;
